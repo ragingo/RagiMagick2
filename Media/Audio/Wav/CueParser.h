45d21dbfab5324f147690e0ece66bd7cdc0f2897
ragingo <4784032+ragingo@users.noreply.github.com>
2025-05-24 00:08:20 +0900

    スペース区切り文字列1行用パーサーを、Cueファイルパーサーに変更

---
+++ Media/Audio/Wav/CueParser.h
@@ -0,0 +1,118 @@
+﻿#pragma once
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Cue.h"
+
+namespace RagiMagick2::Audio::Wav
+{
+    class CueParser final
+    {
+    public:
+        CueParser(std::string_view fileName) noexcept
+            : m_FileName(fileName)
+        {
+            m_Reader = std::ifstream(fileName.data(), std::ios_base::binary);
+        }
+
+        void parse() noexcept
+        {
+            assert(m_Reader);
+
+            std::array<uint8_t, 3> bom;
+            m_Reader.read(reinterpret_cast<char*>(bom.data()), bom.size());
+            if (bom != std::array<uint8_t, 3>{ 0xef, 0xbb, 0xbf }) {
+                m_Reader.seekg(0, std::ios_base::beg);
+            }
+
+            Cue cue{};
+            std::string line;
+            while (std::getline(m_Reader, line)) {
+                auto&& values = parseLine(line);
+
+                std::cout << "======= debug: " << line << std::endl;
+                for (auto&& value : values) {
+                    std::cout << value << std::endl;
+                }
+            }
+        }
+
+    private:
+        std::vector<std::string> parseLine(std::string_view line) noexcept
+        {
+            const std::string_view::const_pointer head = line.data();
+            const std::string_view::const_pointer tail = head + line.size();
+            std::string_view::const_pointer ptr = head;
+            std::string_view::const_pointer startPtr = head;
+            std::string_view::const_pointer endPtr = nullptr;
+
+            std::vector<std::string> fields;
+            bool beginTrim = true;
+            bool isQuotedValue = false;
+
+            while (ptr < tail) {
+                if (*ptr == ' ' && beginTrim) {
+                    ++ptr;
+                    startPtr = ptr;
+                    continue;
+                }
+                else {
+                    beginTrim = false;
+                }
+
+                if (*ptr == '\r') {
+                    if (auto next = ptr + 1; next < tail && *next == '\n') {
+                        ++ptr;
+                        continue;
+                    }
+                }
+
+                switch (*ptr) {
+                case '\r':
+                case '\n':
+                    if (ptr != startPtr) {
+                        endPtr = ptr - 1;
+                    }
+                    break;
+                case '\"':
+                    isQuotedValue = !isQuotedValue;
+                    if (isQuotedValue) {
+                        startPtr = ptr;
+                    }
+                    else {
+                        endPtr = ptr;
+                    }
+                    break;
+                case ' ':
+                    if (!isQuotedValue) {
+                        endPtr = ptr - 1;
+                    }
+                    break;
+                default:
+                    break;
+                }
+
+                if (startPtr && endPtr) {
+                    fields.emplace_back(line.substr(startPtr - head, (endPtr - startPtr) + 1));
+                    startPtr = ptr + 1;
+                    endPtr = nullptr;
+                    beginTrim = true;
+                }
+
+                ++ptr;
+            }
+
+            return fields;
+        }
+
+    private:
+        std::string_view m_FileName;
+        std::ifstream m_Reader;
+    };
+
+}
--- Media/Audio/Wav/WavParser.cpp
+++ Media/Audio/Wav/WavParser.cpp
@@ -4,93 +4,9 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <ranges>
 #include <string>
-#include <vector>
 #include "Cue.h"
-
-
-// Space Separated Value Line Parser
-// スペース区切りの行に特化したパーサー
-class SSVLineParser final
-{
-public:
-    SSVLineParser(const std::string& separator = " ") noexcept
-        : m_Separator(separator)
-    {
-    }
-
-    std::vector<std::string> parse(std::string_view text) noexcept
-    {
-        const std::string_view::const_pointer head = text.data();
-        const std::string_view::const_pointer tail = head + text.size();
-        std::string_view::const_pointer ptr = head;
-        std::string_view::const_pointer startPtr = head;
-        std::string_view::const_pointer endPtr = nullptr;
-
-        std::vector<std::string> tokens;
-        bool beginTrim = true;
-        bool isQuotedValue = false;
-
-        while (ptr < tail) {
-            if (*ptr == ' ' && beginTrim) {
-                ++ptr;
-                startPtr = ptr;
-                continue;
-            }
-            else {
-                beginTrim = false;
-            }
-
-            if (*ptr == '\r') {
-                if (auto next = ptr + 1; next < tail && *next == '\n') {
-                    ++ptr;
-                    continue;
-                }
-            }
-
-            switch (*ptr) {
-            case '\r':
-            case '\n':
-                if (ptr != startPtr) {
-                    endPtr = ptr - 1;
-                }
-                break;
-            case '\"':
-                isQuotedValue = !isQuotedValue;
-                if (isQuotedValue) {
-                    startPtr = ptr;
-                }
-                else {
-                    endPtr = ptr;
-                }
-                break;
-            case ' ':
-                if (!isQuotedValue) {
-                    endPtr = ptr - 1;
-                }
-                break;
-            default:
-                break;
-            }
-
-            if (startPtr && endPtr) {
-                tokens.emplace_back(text.substr(startPtr - head, (endPtr - startPtr) + 1));
-                startPtr = ptr + 1;
-                endPtr = nullptr;
-                beginTrim = true;
-            }
-
-            ++ptr;
-        }
-
-        return tokens;
-    }
-
-private:
-    std::string m_Separator;
-};
-
+#include "CueParser.h"
 
 namespace RagiMagick2::Audio::Wav
 {
@@ -109,32 +25,8 @@ namespace RagiMagick2::Audio::Wav
         }
 
         if (!m_CueFileName.empty()) {
-            parseCue();
-        }
-    }
-
-    void WavParser::parseCue() noexcept
-    {
-        assert(!m_CueFileName.empty());
-
-        std::ifstream reader(m_CueFileName.data(), std::ios_base::binary);
-
-        std::array<uint8_t, 3> bom;
-        reader.read(reinterpret_cast<char*>(bom.data()), bom.size());
-        if (bom != std::array<uint8_t, 3>{ 0xef, 0xbb, 0xbf }) {
-            reader.seekg(0, std::ios_base::beg);
-        }
-
-        Cue cue{};
-        std::string line;
-        SSVLineParser lineParser;
-        while (std::getline(reader, line)) {
-            auto values = lineParser.parse(line);
-
-            std::cout << "===== debug: " << line << std::endl;
-            for (auto&& value : values) {
-                std::cout << value << std::endl;
-            }
+            CueParser parser(m_CueFileName);
+            parser.parse();
         }
     }
 
